fca62080ca1b3d5e4bc4d34137a36eafcb2fc1b1
PENGYUXIONG <pxiong@ualberta.ca>
2021-01-17 02:59:34 -0700

    build up init server

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #  coding: utf-8 
 import socketserver
-
+import os
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,12 +27,80 @@ import socketserver
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
-class MyWebServer(socketserver.BaseRequestHandler):
+class msgParser():
+    def getRequestAddr(self, defaultStr):
+        tokenList = defaultStr.split(' ')[:2]
+        httpMethod = tokenList[0]
+        route = tokenList[1]
+        if route:
+            return httpMethod, route
     
+    def getRespond(self, httpMethod, endPoint):
+        if httpMethod != 'GET':
+            return self.formatResponse('', None, 405)
+
+        response = None
+        fileType = None
+        path = os.getcwd()
+
+        if endPoint[0:4] != '/www':
+            path = path + '/www'
+        path += endPoint
+        path = os.path.normpath(path)
+
+        if endPoint[-1] != '/' and endPoint[-4:] != '.css':
+            response = self.formatResponse('http://127.0.0.1:8080' + endPoint + '/', None, 301)
+        elif os.path.exists(path) and os.getcwd() in path:
+            if not os.path.isfile(path):
+                fileType = 'html'
+                path = os.path.join(path, 'index.html')
+            else:
+                if path[-4:] == 'html':
+                    fileType = 'html'
+                else:
+                    fileType = 'css'
+            f = open(path, 'r')
+            content = f.read()
+            f.close()
+            response = self.formatResponse(content, fileType, 200)
+        else:
+            response = self.formatResponse('', None, 404)
+        return response
+
+    def formatResponse(self, content, fileType, statusCode):
+        header = ''
+        if statusCode == 200:
+            header = "HTTP/1.1 200 OK\n"
+            if (fileType == 'html'):
+                header += "Content-Type: text/html\n\n"
+            elif (fileType == 'css'):
+                header += "Content-Type: text/css\n\n"
+        elif statusCode == 405:
+            content = "HTTP/1.1 405 Method Not Allowed\n"
+        elif statusCode == 404:
+            content = "HTTP/1.1 404 Page Not Found\n"
+        elif statusCode == 301:
+            header = "HTTP/1.1 301 Moved Permanently\n"
+            content = "Location: " + content + "\n\n"
+
+        finalRes = header + content
+        return finalRes
+
+class MyWebServer(socketserver.BaseRequestHandler):
+    # initialize string parser
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(1024).decode('utf8')
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        newMsgParser = msgParser()
+        httpMethod, route = newMsgParser.getRequestAddr(self.data)
+
+        response = newMsgParser.getRespond(httpMethod, route)
+
+        if not response:
+            return
+        else:
+            self.request.sendall(bytearray(response,'utf-8'))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
